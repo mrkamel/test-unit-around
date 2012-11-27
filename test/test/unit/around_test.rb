94f424ed4d1b8f5f78a14a66ba72641eae81ddca
Benjamin Vetter <vetter@flakks.com>
2012-11-27 14:51:13 +0100

    Tests extended

--- test/test/unit/around_test.rb
+++ test/test/unit/around_test.rb
@@ -3,42 +3,62 @@ require File.expand_path("../../../../lib/test/unit/around", __FILE__)
 require "test/unit"
 
 class AroundClassMethodTest < Test::Unit::TestCase
+  setup do
+    assert_nil @before
+    assert_nil @after
+  end
+
   around do |test|
     @before = :before
 
     test.run
 
     @after = :after
+  end
 
+  teardown do
+    assert_equal :before, @before
     assert_equal :after, @after
   end
 
-  def test_around
+  test "around" do
     assert_equal :before, @before
+
     assert_nil @after
   end
 end
 
 class AroundInstanceMethodTest < Test::Unit::TestCase
+  def setup
+    assert_nil @before
+    assert_nil @after
+  end
+
   def around
     @before = :before
 
     yield
 
     @after = :after
+  end
 
+  def teardown
+    assert_equal :before, @before
     assert_equal :after, @after
   end
 
   def test_around
     assert_equal :before, @before
+
     assert_nil @after
   end
 end
 
 class WithoutAroundTest < Test::Unit::TestCase
   def test_something
+    assert_nil @before
     assert_nil @after
+
     assert_equal 1, 1
   end
 end
